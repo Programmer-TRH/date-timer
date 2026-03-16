c85da9676c6164f08c1c605a0ff42095c451a2c0
Programmer-TRH <tokdirrahoman4@gmail.com>
2026-03-16 15:43:34 +0600

    v-0.0.6

--- schemas/booking-schema.ts
+++ schemas/booking-schema.ts
@@ -1,9 +1,21 @@
 import { z } from "zod";
 
 export const bookingSchema = z.object({
-  country: z.string().min(1, "Please select a country"),
-  state: z.string().min(1, "Please select a state"),
-  city: z.string().min(1, "Please select a city"),
+  country: z
+    .string({
+      message: "Please select a country",
+    })
+    .min(1, "Please select a country"),
+  state: z
+    .string({
+      message: "Please select a state",
+    })
+    .min(1, "Please select a state"),
+  city: z
+    .string({
+      message: "Please select a state first then city",
+    })
+    .min(1, "Please select a city"),
 });
 
 export type BookingFormData = z.infer<typeof bookingSchema>;
